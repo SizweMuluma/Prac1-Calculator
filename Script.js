3d576be6ec2c6f14dd4ac3c3ad0ebc28eecf5bc7
Sizwe <sizwe@muluma.co.za>
2024-06-12 15:49:11 +0200

    first commit

---
+++ Script.js
@@ -0,0 +1,40 @@
+const add = ()=>{
+
+    let input1;
+    let input2;
+
+    input1 = document.querySelector("#input1").value
+    input2 = document.querySelector("#input2").value
+
+    let result =  +input1 + +input2;
+
+    // alert (`Result :  ${result}`);
+    console.log("Result " +  result);
+    document.querySelector("#resultValue").innerHTML = result
+
+}
+
+// Minus
+const minus = ()=>{
+    let input1;
+    let input2;
+
+    input1 = document.querySelector("#input1").value
+    input2 = document.querySelector("#input2").value
+
+    let result =  +input1 - +input2;
+
+    // alert (`Result :  ${result}`);
+    console.log("Result " +  result);
+    document.querySelector("#resultValue").innerHTML = result
+}
+
+// Divide
+const dinus = ()=>{
+
+}
+
+// Multiply
+const multiply = ()=>{
+
+}
